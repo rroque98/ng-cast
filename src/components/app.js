8fc565ddbdcdd2a8d41fe535911a59064aecee77
rroque98 <rroque98@gmail.com>
2018-06-29 19:31:43 -0700

    videoPlayer specs passed

--- src/components/app.js
+++ src/components/app.js
@@ -4,10 +4,16 @@ angular.module('video-player')
   // TODO
     controller: function() {
       this.videos = window.exampleVideoData;
-      this.video = window.exampleVideoData[0];
+      this.video = this.videos[0];
 
       this.onClick = () => {
         console.log('clicked');
+      };
+      this.selectVideo = () => {
+        console.log('video selected!');
+      };
+      this.searchResults = () => {
+        console.log('video search results!');
       };
     },
     templateUrl: 'src/templates/app.html' 
